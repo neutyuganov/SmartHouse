8d771913a1e35ea8107e8d905948c615feb3b9b9
utyuganov <utyuganovsergey55@gmail.com>
2023-11-22 23:25:29 +0600

    Добавлены экраны устройств комнаты, добавления устройств, профиля

---
+++ app/src/main/java/com/example/smarthouse/AddDeviceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smarthouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddDeviceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_device_activity)
+    }
+}
---
+++ app/src/main/java/com/example/smarthouse/DevicesInRoomActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smarthouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DevicesInRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.devices_in_room_activity)
+    }
+}
