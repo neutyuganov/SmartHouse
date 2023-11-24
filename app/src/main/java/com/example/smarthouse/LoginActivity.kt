61e04f3c94573b7de4f5e052d00d8083d5b5916a
utyuganov <utyuganovsergey55@gmail.com>
2023-11-25 02:14:08 +0600

    Сделан вход и доработана регистрация с учетом ошибки на отсутствие подключения к интернету

--- app/src/main/java/com/example/smarthouse/LoginActivity.kt
+++ app/src/main/java/com/example/smarthouse/LoginActivity.kt
@@ -1,12 +1,32 @@
 package com.example.smarthouse
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import io.github.jan.supabase.gotrue.gotrue
+import io.github.jan.supabase.gotrue.providers.builtin.Email
+import io.github.jan.supabase.postgrest.postgrest
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
+
+    // Объявление переменной класса
+    private val networkMonitor = NetworkMonitorUtil(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val window = window
@@ -16,17 +36,175 @@ class LoginActivity : AppCompatActivity() {
         )
         setContentView(R.layout.login_activity)
 
+        //Подключение клиента SupaBase
+        val clientSB = CreateClientSB().clientSB
+
+        // Создание переменной Vibratior
+        val vibrator = this.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        // Создание переменной TextView
+        val text_view_error: TextView = findViewById(R.id.login_textViewError)
+
+        // Создание переменных EditText
+        val edit_text_email: EditText = findViewById(R.id.login_email)
+        val edit_text_password: EditText = findViewById(R.id.login_password)
+
+        // Создание переменных Button
         val goLogin: Button = findViewById(R.id.login_button_go_login)
         val goRegistration: Button = findViewById(R.id.login_button_go_reg)
 
+        // Обработчик нажатия кнопки Регистрация
         goRegistration.setOnClickListener{
             val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
         }
 
+        // Функция вибрации 500 мс
+        fun isVibrationError(){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                // API 26
+                vibrator.vibrate(
+                    VibrationEffect.createOneShot(
+                        500,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                )
+            } else {
+                // This method was deprecated in API level 26
+                vibrator.vibrate(500)
+            }
+        }
+
+        // Постоянная проверка подключения к интернету
+        networkMonitor.result = { isAvailable, type ->
+            runOnUiThread {
+                when (isAvailable) {
+                    true -> {
+                        text_view_error.visibility = TextView.INVISIBLE
+                    }
+                    false -> {
+
+                    }
+                }
+            }
+        }
+
+        // Функция проверки подключения к интернету
+        fun isInternetConnected(context: Context): Boolean {
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val network = connectivityManager.activeNetwork ?: return false
+                val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+                return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            } else {
+                val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+                return networkInfo.isConnected
+            }
+        }
+
+        // Функция валидации логина
+        fun isValidEmail(email: String): Boolean {
+            val regex = Regex("[a-z0-9._-]+@[a-z0-9.-]+\\.[a-z]{2,}")
+            return regex.matches(email)
+        }
+
+        // Функция проверки полей входа
+        fun isEditTextsCheck(){
+            //Объявление переменных с данными из EditText
+            val email_user = edit_text_email.text.toString()
+            val password_user = edit_text_password.text.toString()
+
+            text_view_error.visibility = TextView.INVISIBLE
+
+            if(email_user == "" || password_user == "") {
+                goLogin.isEnabled = false
+            }
+            else goLogin.isEnabled = true
+        }
+
+        // Проверка полей ввода после каждого введенного символа в EditText
+        edit_text_email.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                isEditTextsCheck()
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+        edit_text_password.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                isEditTextsCheck()
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        // Обработчик нажатия кнопки Вход
         goLogin.setOnClickListener{
-            val intent = Intent(this, PinCodeActivity::class.java)
-            startActivity(intent)
+            try{
+                //Объявление переменных с данными из EditText
+                val email_user = edit_text_email.text.toString()
+                val password_user = edit_text_password.text.toString()
+
+                // Функция Входа
+                fun isLogin(){
+                    lifecycleScope.launch {
+                        try {
+                            clientSB.gotrue.loginWith(Email) {
+                                email = email_user
+                                password = password_user
+                            }
+
+                            // Переход на форму ввода пинкода
+                            val intent =
+                                Intent(this@LoginActivity, PinCodeActivity::class.java)
+                            startActivity(intent)
+                        } catch (e:Exception)
+                        {
+                            isVibrationError()
+                            text_view_error.visibility = TextView.VISIBLE
+
+                            // Проверка условий для вывод ошибок в TextView
+                            if(isInternetConnected(this@LoginActivity)){
+                                text_view_error.setText("Неверная почта или пароль")
+                            }
+                            else {
+
+                                text_view_error.setText("Нет подключения к интернету")
+                            }
+                        }
+                    }
+                }
+
+                // Проверка условий для вывода ошибок в EditText
+                when{
+                    !isValidEmail(email_user) && password_user.length < 6 -> {
+                        edit_text_email.setError("Неверный формат почты")
+                        edit_text_password.setError("Пароль должен быть не меньше 6 символов")
+                    }
+                    !isValidEmail(email_user) -> edit_text_email.setError("Неверный формат почты")
+                    password_user.length < 6 -> edit_text_password.setError("Пароль не может быть меньше 6 символов")
+                    isValidEmail(email_user) && password_user.length >= 6 -> isLogin()
+                }
+            }
+            catch (e:Exception) {
+                Log.e("ERROR!!!!", e.toString())
+            }
+
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        networkMonitor.register()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        networkMonitor.unregister()
+    }
+
 }
--- app/src/main/java/com/example/smarthouse/RegistrationActivity.kt
+++ app/src/main/java/com/example/smarthouse/RegistrationActivity.kt
@@ -1,7 +1,13 @@
 package com.example.smarthouse
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -9,6 +15,7 @@ import android.util.Patterns
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import io.github.jan.supabase.gotrue.gotrue
@@ -17,6 +24,10 @@ import io.github.jan.supabase.postgrest.postgrest
 import kotlinx.coroutines.launch
 
 class RegistrationActivity : AppCompatActivity() {
+
+    // Объявление переменной класса
+    private val networkMonitor = NetworkMonitorUtil(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val window = window
@@ -28,6 +39,12 @@ class RegistrationActivity : AppCompatActivity() {
 
         //Подключение клиента SupaBase
         val clientSB = CreateClientSB().clientSB
+
+        // Создание переменной Vibratior
+        val vibrator = this.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        // Создание переменной TextView
+        val text_view_error: TextView = findViewById(R.id.registration_textViewError)
 
         // Создание переменных EditText
         val edit_text_name: EditText = findViewById(R.id.registration_name)
@@ -44,6 +61,49 @@ class RegistrationActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // Функция вибрации 500 мс
+        fun isVibrationError(){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                // API 26
+                vibrator.vibrate(
+                    VibrationEffect.createOneShot(
+                        500,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                )
+            } else {
+                // This method was deprecated in API level 26
+                vibrator.vibrate(500)
+            }
+        }
+
+        // Постоянная проверка подключения к интернету
+        networkMonitor.result = { isAvailable, type ->
+            runOnUiThread {
+                when (isAvailable) {
+                    true -> {
+                        text_view_error.visibility = TextView.INVISIBLE
+                    }
+                    false -> {
+
+                    }
+                }
+            }
+        }
+
+        // Функция проверки подключения к интернету
+        fun isInternetConnected(context: Context): Boolean {
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val network = connectivityManager.activeNetwork ?: return false
+                val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+                return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            } else {
+                val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+                return networkInfo.isConnected
+            }
+        }
+
         // Функция валидации логина
         fun isValidEmail(email: String): Boolean {
             val regex = Regex("[a-z0-9._-]+@[a-z0-9.-]+\\.[a-z]{2,}")
@@ -57,12 +117,15 @@ class RegistrationActivity : AppCompatActivity() {
             val email_user = edit_text_email.text.toString()
             val password_user = edit_text_password.text.toString()
 
+            text_view_error.visibility = TextView.INVISIBLE
+
             if(name_user == "" || email_user == "" || password_user == "") {
                 goRegistration.isEnabled = false
             }
             else goRegistration.isEnabled = true
         }
 
+        // Проверка полей ввода после каждого введенного символа в EditText
         edit_text_name.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
@@ -72,7 +135,6 @@ class RegistrationActivity : AppCompatActivity() {
 
             override fun afterTextChanged(s: Editable) {}
         })
-
         edit_text_email.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
@@ -82,7 +144,6 @@ class RegistrationActivity : AppCompatActivity() {
 
             override fun afterTextChanged(s: Editable) {}
         })
-
         edit_text_password.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
@@ -96,33 +157,49 @@ class RegistrationActivity : AppCompatActivity() {
         // Обработчик нажатия кнопки Регистриция
         goRegistration.setOnClickListener{
             try{
-                //Объявление переменных с данными из EdtText
+                //Объявление переменных с данными из EditText
                 val name_user = edit_text_name.text.toString()
                 val email_user = edit_text_email.text.toString()
                 val password_user = edit_text_password.text.toString()
 
                 fun isRegistration(){
                     lifecycleScope.launch {
-                        clientSB.gotrue.signUpWith(Email) {
-                            email = email_user
-                            password = password_user
+                        try {
+                            clientSB.gotrue.signUpWith(Email) {
+                                email = email_user
+                                password = password_user
+                            }
+
+                            // Ввод данных в таблицу Users
+                            val user =
+                                clientSB.gotrue.retrieveUserForCurrentSession(updateSession = true)
+                            val userAdd = UserDataClass(id = user.id, name = name_user)
+                            clientSB.postgrest["Users"].insert(userAdd)
+
+                            // Переход на форму ввода пинкода
+                            val intent =
+                                Intent(this@RegistrationActivity, PinCodeActivity::class.java)
+                            startActivity(intent)
+                        } catch (e:Exception)
+                        {
+                            isVibrationError()
+                            text_view_error.visibility = TextView.VISIBLE
+
+                            // Проверка условий для вывод ошибок в TextView
+                            if(!isInternetConnected(this@RegistrationActivity)){
+                                text_view_error.setText("Нет подключения к интернету")
+                            }
                         }
-
-                        val user =
-                            clientSB.gotrue.retrieveUserForCurrentSession(updateSession = true)
-                        val userAdd = UserDataClass(id = user.id, name = name_user)
-                        clientSB.postgrest["Users"].insert(userAdd)
-
-                        // Переход на форму ввода пинкода
-                        val intent =
-                            Intent(this@RegistrationActivity, PinCodeActivity::class.java)
-                        startActivity(intent)
                     }
                 }
 
                 when{
+                    !isValidEmail(email_user) && password_user.length < 6 -> {
+                        edit_text_email.setError("Неверный формат почты")
+                        edit_text_password.setError("Пароль должен быть не меньше 6 символов")
+                    }
                     !isValidEmail(email_user) -> edit_text_email.setError("Неверный формат почты")
-                    password_user.length < 6 -> edit_text_password.setError("Пароль может быть не меньше 6 символов")
+                    password_user.length < 6 -> edit_text_password.setError("Пароль должен быть не меньше 6 символов")
                     isValidEmail(email_user) && password_user.length >= 6 -> isRegistration()
                 }
             }
@@ -132,4 +209,15 @@ class RegistrationActivity : AppCompatActivity() {
 
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        networkMonitor.register()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        networkMonitor.unregister()
+    }
+
 }
